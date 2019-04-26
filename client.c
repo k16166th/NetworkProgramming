ed74c72beccb6a3f0dba0352715a580ca30b79c8
4423_樋口 拓真 <k16166th>
2019-04-26 10:25:04 +0900

    クライアント作成

--- client.c
+++ client.c
@@ -1,6 +1,19 @@
-//#include<stdio.h>
+#include<stdio.h>
+#include<string.h>
+#include<unistd.h>    //close()を使うため
 #include<sys/socket.h>
 #include<arpa/inet.h>
+
+void commun(int sock){
+	char buf[256];
+	int len_r;       //受信文字数
+	char *message="チャチャタウン小倉（チャチャタウンこくら）は、福岡県北九州市小倉北区砂津にある西日本鉄道経営の複合商業施設。通称「チャチャタウン」、「チャチャ」。";
+	send(sock, message, strlen(message), 0);
+	len_r=recv(sock, buf, 256, 0);
+	buf[len_r]='\0';
+	
+	printf("%s\n", buf);
+}
 
 int main(int argc, char **argv){
 	int sock=socket(PF_INET,SOCK_STREAM,0);
@@ -12,6 +25,7 @@ int main(int argc, char **argv){
 	target.sin_port=htons(10001);
 	
 	connect(sock,(struct sockaddr*)&target, sizeof(target));
+	commun(sock);
 	
 	close(sock);
 	
