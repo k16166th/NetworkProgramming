76db7c200c67491d6c4cf77e9691743dc976445c
k16166th <49297420+k16166th@users.noreply.github.com>
2019-10-17 16:12:55 +0900

    Add files via upload

--- 3-Message/client.c
+++ 3-Message/client.c
@@ -45,13 +45,60 @@ int prepare_client_socket(char *ipaddr, int port)
     target.sin_family = AF_INET;
     target.sin_addr.s_addr = inet_addr(ipaddr);
     target.sin_port = htons(port);
-    if(connect(sock, (struct sockaddr*)&target, sizeof(target)) < 0)
+    if (connect(sock, (struct sockaddr *)&target, sizeof(target)) < 0)
         DieWithError("connect() failed");
 
     return sock;
 }
+
+void my_scanf(char *buf, int num_letter)
+{
+    //num_letter文字だけ入力させる（scanfの脆弱性対策）
+    char format[20];
+    sprintf(format, "%s%d%s", "%", num_letter, "s%*[^\n]");
+    scanf(format, buf);
+    getchar();
+}
+
+void commun(int sock)
+{
+    char cmd[2] = "";                    //コマンド入力用
+    char withdraw[MONEY_DIGIT_SIZE + 1]; //引き出し額
+    char deposit[MONEY_DIGIT_SIZE + 1];  //預け入れ額
+    char msg[BUF_SIZE];                  //送信メッセージ
+
+    printf("0:引き出し 1:預け入れ 2:残高照会\n");
+    printf("何をしますか？ > ");
+
+    my_scanf(cmd, 1);
+
+    switch (cmd[0])
     {
-        /* data */
-    };
-    )))
+    case '0':
+        //引き出し処理
+        printf("引き出す金額を入力してください > ");
+        my_scanf(withdraw, MONEY_DIGIT_SIZE);
+
+        sprintf(msg, "0_%s_", withdraw);
+        break;
+    case '1':
+        //預け入れ処理
+        printf("預け入れる金額を入力してください > ");
+        my_scanf(deposit, MONEY_DIGIT_SIZE);
+
+        sprintf(msg, "0_%s_", deposit);
+        break;
+    case '2':
+        //残高照会
+        strcpy(msg, "0_0_");
+        break;
+    default:
+        //unknownエラー、終了
+        printf("番号が確認できませんでした。\n");
+        return;
+    }
+    //送信処理
+    if (send(sock, msg, strlen(msg), 0) != strlen(msg))
+        DieWithError("send() sent a message of unexpected bytes");
+    //受信処理
 }
