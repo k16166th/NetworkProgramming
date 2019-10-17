563735a3c49ae84421b01dca6abd6709f95874b5
k16166 <k16166th@apps.kct.ac.jp>
2019-10-17 15:59:52 +0900

    20191017

--- 3-Message/server.c
+++ 3-Message/server.c
@@ -1,3 +1,5 @@
+//server ATM
+
 #include <stdio.h>
 #include <string.h>
 #include <sys/socket.h>
@@ -11,6 +13,9 @@
 void DieWithError(char *);
 int prepare_server_socket(int);
 void commun(int);
+void read_until_delim(int, char *, int);
+int get_current_balance();
+void set_current_balance(int);
 
 int main(int argc, char *argv[])
 {
@@ -56,4 +61,58 @@ int prepare_server_socket(int port)
     bind(servSock, (struct sockaddr *)&servAddress, sizeof(servAddress));
 
     return servSock;
+}
+
+void read_until_delim(int sock, char *buf, char delimiter, int max_length)
+{
+    int len_r = 0;        // 受信文字数
+    int index_letter = 0; //受信文字数の合計
+
+    while (index_letter < max_length - 1)
+    {
+        //１文字だけ受信
+
+        if ((len_r = recv(sock, buf + index_letter, 1, 0)) <= 0)
+        {
+            //エラー
+            printf("接続が切れました\n");
+            return;
+        }
+
+        if (buf[index_letter] == delimiter)
+            //区切り文字を受信→ループを抜ける
+            break;
+        else
+            index_letter++;
+    }
+
+    //nullを末尾に追加
+    buf[index_letter] = '\0';
+}
+
+int get_current_balance()
+{
+    return 1000000;
+}
+
+void set_current_balance(int new_balance)
+{
+    return;
+}
+
+void commun(int sock)
+{
+    char buf[BUF_SIZE];
+    int balance = get_current_balance();
+
+    read_until_delim(sock, buf, '_', BUF_SIZE);
+    balance += atoi(buf); //預け入れ金額をプラス
+    read_until_delim(sock, buf, '_', BUF_SIZE);
+    balance -= atoi(buf); //引き出し金額をマイナス
+
+    set_current_balance(balance);
+
+    sprintf(buf, "%d_", balance);
+    if (send(sock, buf, strlen(buf), 0) != strlen(buf))
+        DieWithError("send() sent a message of unexpected bytes");
 }
